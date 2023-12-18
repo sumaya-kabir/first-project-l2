ec14c5ed0133f855259f75a4957fda11acf7083c
sumaya-kabir <sumayakabir4547@gmail.com>
2023-12-18 16:10:38 +0600

    update in student-validation

--- src/app/modules/users/user.route.ts
+++ src/app/modules/users/user.route.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 import { UserController } from './user.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { createStudentValidationSchema } from '../student/student.validation';
 
 const router = express.Router();
 
-router.post('/create-student', UserController.createStudent);
+router.post(
+  '/create-student',
+  validateRequest(createStudentValidationSchema),
+  UserController.createStudent,
+);
 
 export const UserRoutes = router;
