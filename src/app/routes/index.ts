71c2dbaf9a2ff74b8b7699dbf135505fc34771af
sumaya-kabir <sumayakabir4547@gmail.com>
2023-12-21 11:38:08 +0600

    academic-department added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import { UserRoutes } from '../modules/users/user.route';
 import { StudentRoutes } from '../modules/student/student.route';
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.route';
+import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route';
+import { AcademicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.route';
 
 const router = Router();
 
@@ -17,6 +19,14 @@ const moduleRoutes = [
   {
     path: '/academic-semester',
     route: AcademicSemesterRoutes,
+  },
+  {
+    path: '/academic-faculties',
+    route: AcademicFacultyRoutes,
+  },
+  {
+    path: '/academic-departments',
+    route: AcademicDepartmentRoutes,
   },
 ];
 
